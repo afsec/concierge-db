7d0d5e9c066e55ab1d0bbb1dab02e72856435dc1
Antonio Feitosa <antonio.cfc@gmail.com>
2020-06-02 18:24:52 -0300

    Fix deploy scripts

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
+# Staging image (busybox:musl)
+# FROM busybox:musl
+
+# Production image (scratch)
 FROM scratch
+ENV PIPELINE development
 COPY ./dist/concierge-db /
 COPY ./dist/database.sqlite3 /
 EXPOSE 3341
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,20 +3,12 @@
 PACKAGE_NAME=$(head Cargo.toml | awk '/^name/{print $3}' | tr -d '"' | tr -d "'")
 IMAGE_NAME=$(git remote get-url --all origin | cut -d ':' -f 2 | sed "s/\..*$//g")
 
-cargo build --release --target $(rustup target list | awk '/musl.*installed/{print $1}')
-mkdir -p ./dist
-cp -v ./target/x86_64-unknown-linux-musl/release/${PACKAGE_NAME} ./dist/${PACKAGE_NAME}
-strip ./dist/${PACKAGE_NAME}
-
 deploy() {
 	docker build -t ${IMAGE_NAME} .
-#	docker run --name ${PACKAGE_NAME} -d -p 8000:8000 ${IMAGE_NAME}
-#	docker logs -f ${PACKAGE_NAME}
     exit 0
 }
 
 undeploy() {
-#	docker rm -f ${PACKAGE_NAME}
 	docker rmi ${IMAGE_NAME}:latest
     exit 0
 }
